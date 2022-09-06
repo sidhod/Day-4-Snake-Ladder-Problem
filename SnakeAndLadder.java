91c21ceee8a6065154e65e405c8fe83aa6ed9c47
unknown <sidhodhank2018@gmail.com>
2022-09-06 11:52:47 +0530

    UC-1 Set Starting Position and Name Of Single Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,13 +1,21 @@
 package Basics;
+import java.util.*;
 
 public class SnakeAndLadder {
+	
 	public static void main(String[] args) {
+		int Start_Position=0;
     	System.out.println(""
     			+ "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
     			+ " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
     			+ "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
     			+ " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
     			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
-    			+ "-------------------SNAKE AND LADDER GAME-----------------------");
+    			+ "-------------------SNAKE AND LADDER GAME-----------------------\r\n");
+    	Scanner name= new Scanner(System.in); //System.in is a standard input stream.
+    	System.out.print("Enter Your Name- ");
+    	String PlayerName= name.nextLine();
+    	System.out.println("Starting Position Of Player"+"("+ PlayerName +"): "+ Start_Position);
     }
 }
+
