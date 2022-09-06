0dae8fadf38c90c62aedf3b71febeab0d286a790
unknown <sidhodhank2018@gmail.com>
2022-09-06 13:46:18 +0530

    Player Rolls Die between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,3 +1,7 @@
+/*welcome message
+ * Take player name from user input
+ * print starting position of player
+ * player rolls die to get value*/
 package Basics;
 import java.util.*;
 
@@ -5,6 +9,7 @@ public class SnakeAndLadder {
 	
 	public static void main(String[] args) {
 		int Start_Position=0;
+		int Die=0;
     	System.out.println(""
     			+ "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
     			+ " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
@@ -13,9 +18,14 @@ public class SnakeAndLadder {
     			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     			+ "-------------------SNAKE AND LADDER GAME-----------------------\r\n");
     	Scanner name= new Scanner(System.in); //System.in is a standard input stream.
-    	System.out.print("Enter Your Name- ");
+    	System.out.print("Enter Your Name-: ");
     	String PlayerName= name.nextLine();
     	System.out.println("Starting Position Of Player"+"("+ PlayerName +"): "+ Start_Position);
+    	int DieValue = (int) ((Math.random()*(7-1)) + 1);
+    	System.out.println("Die Thrown Value: "+ DieValue);
+    	
+    
+    
     }
 }
 
