9f4e68c4137255801c1324122a169014bfa7049b
unknown <sidhodhank2018@gmail.com>
2022-09-07 19:10:28 +0530

    Player Get Exact Winning Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,7 @@
  * Add No Play ,Snake and Ladder Case
  * Repeat till the Player reaches the winning position 100.
  * position moves below 0, then the player restarts from 0.
+ * Set Winning position user position 100
  */
 package Basics;
 import java.util.*;
@@ -12,7 +13,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		SnakeAndLadder obj = new SnakeAndLadder();
 		int startPosition=0;
-	int userPosition = 1;
+		int temp=0;
+	int userPosition = 0;
 		int n=0;
 		String sGame = "r";
     	System.out.println(""
@@ -22,32 +24,36 @@ public class SnakeAndLadder {
     			+ " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
     			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
     			+ "-------------------SNAKE AND LADDER GAME-----------------------\r\n");
-    	Scanner name= new Scanner(System.in); //System.in is a standard input stream.
+    	Scanner name= new Scanner(System.in); 
     	System.out.print("Enter Your Name-: ");
     	String playerName= name.nextLine();
     	System.out.println("Starting Position Of Player"+"("+ playerName +"): "+ startPosition);
     	
-    	while(sGame.equals ("r") || sGame.equals ("R")) {
+    	while(sGame.equals ("r") || sGame.equals ("R")){
     	int DieValue = (int) ((Math.random()*(7-1)) + 1);
-    	
-    	userPosition = userPosition+DieValue;
+    	userPosition=userPosition+DieValue;
     	userPosition= obj.checkCases(userPosition,DieValue);
+    	
         displayOutput(userPosition,DieValue,playerName);
+         if (userPosition == 100)
+        {
+        	sGame="s";
+            System.out.println ("YOU WON, GOOD JOB!!!");
+            
+        }
+         else {
     	System.out.println("Press 'R' for Die Thrown");
         Scanner Game= new Scanner(System.in);
-        if(userPosition <= 100)
-        {
         sGame= Game.nextLine();
-        }else {
-        	sGame="s";
-        }
+         }
+        
         
         
         
     	}
 	}
    static void displayOutput(int userPosition,int DieValue,String playerName){
-    	System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+	   System.out.println("---------------------------------------------------------------------------------------------------------------------------");
     	System.out.println("Die Thrown Value: "+ DieValue);
         System.out.println ("\t\t\t\t\t-------------------------------");
         System.out.println ("\t\t\t\t\t|      You Rolled a: " + DieValue + "         |");
@@ -58,11 +64,18 @@ public class SnakeAndLadder {
 	}
 	 public int checkCases(int userPosition,int DieValue) 
 	 {   
-		 if(userPosition < 0) 
+		 	if(userPosition < 0) 
 	        {
 	            userPosition = 0; 
 	            System.out.println ("\t\t\t\t~~~~~~~~~~~~~Come To Start Position!!!~~~~~~~~~~~~~");
 	        }
+		 	 else if (userPosition > 100)
+		        {
+		            userPosition=userPosition-DieValue; 
+		            
+		            
+		        }
+		    
 	        else if(userPosition == 15) 
 	        {
 	            userPosition = userPosition + DieValue; 
@@ -110,39 +123,29 @@ public class SnakeAndLadder {
 	            
 	        }else if(userPosition == 10) 
 	        {
-	            userPosition = userPosition - DieValue; 
-	            System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN!!!~~~~~~~~~~~~~");
+	            userPosition = userPosition - DieValue- DieValue; 
+	            System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN By "+DieValue+"!!!~~~~~~~~~~~~~");
 	        }
 	        else if (userPosition == 27)
 	        {
 	        	userPosition = userPosition-DieValue;
-	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN!!!~~~~~~~~~~~~~");
+	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN By "+DieValue+"!!!~~~~~~~~~~~~~");
 	            
 	        }else if (userPosition == 35)
 	        {
 	        	userPosition = userPosition-DieValue; 
-	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN!!!~~~~~~~~~~~~~");
-	            
+	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN By "+DieValue+"!!!~~~~~~~~~~~~~");
 	        }else if (userPosition == 56)
 	        {
 	        	userPosition = userPosition-DieValue; 
-	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN!!!~~~~~~~~~~~~~");
+	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN By "+DieValue+"!!!~~~~~~~~~~~~~");
 	            
 	        }else if (userPosition == 68)
 	        {
 	        	userPosition = userPosition-DieValue; 
-	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN!!!~~~~~~~~~~~~~");
+	        	System.out.println ("\t\t\t\t~~~~~~~~~~~~~You Got Bit By A Snake, GO DOWN By "+DieValue+"!!!~~~~~~~~~~~~~");
 	            
-	        }
-	        else if (userPosition == 100)
-	        {
-	            System.out.println ("YOU WON, GOOD JOB!!!"); 
-	            
-	            
-	        }
-	        
-	        
-	        
+	        } 
 	        return userPosition; 
 	    }
 	    
